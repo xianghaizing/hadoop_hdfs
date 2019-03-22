4a1dbe993dc5d9f7aa12c2e89550d9098c468cba
haizing <779397435@qq.com>
2019-03-22 16:57:09 +0800

    util

---
+++ src/main/java/com/lyf/mr05/SequenceFileDriver.java
@@ -0,0 +1,36 @@
+package com.lyf.mr05;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+public class SequenceFileDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException,
+            InterruptedException {
+        args = new String[]{"E:\\hadoop_hdfs\\input\\02", "E:\\hadoop_hdfs\\output\\0707"};
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(SequenceFileDriver.class);
+        job.setMapperClass(SequenceFileMapper.class);
+        job.setReducerClass(SequenceFileReducer.class);
+// 设置输入的 inputFormat
+        job.setInputFormatClass(WholeFileInputformat.class);
+// 设置输出的 outputFormat
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(BytesWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(BytesWritable.class);
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
+}
